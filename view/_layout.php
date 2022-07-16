9eeea2926caf370b7a2689f62994562afc9ee135
Gunārs Ābeltiņš <gunars2003@gmail.com>
2022-07-16 12:09:34 +0300

    Make responsive grid design and add footer

--- view/_layout.php
+++ view/_layout.php
@@ -7,12 +7,18 @@
     <title>Product List</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="/public/style.css">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </head>
 
-<body>
+<body class="d-flex flex-column min-vh-100">
 
     <?php echo $content ?>
 
+    <footer class="mt-auto">
+        <div class="mx-5 py-3 text-center border-top border-2 border-primary">
+            Scandiweb Test assignment
+        </div>
+    </footer>
 </body>
 
 </html>
--- view/products/list.php
+++ view/products/list.php
@@ -1,23 +1,29 @@
-<nav class="navbar bg-light">
-    <div class="container-fluid">
-        <p class="navbar-brand">Products List</p>
-        <form class="d-flex">
+<nav class="navbar bg-primary">
+    <div class="container-fluid px-4 ">
+        <h2 class="navbar-brand">Products List</h2>
+        <span>
             <button class="btn btn-outline-dark" type="submit">Add</button>
-            <button id="delete-product-btn" class="btn btn-outline-dark" type="submit">Search</button>
-        </form>
+            <button id="delete-product-btn" class="btn btn-outline-dark" type="submit">Mass delete</button>
+        </span>
     </div>
 </nav>
 
-
-<div class="container overflow-hidden">
-    <div class="row row-cols-4 g-5">
+<div class="container my-5">
+    <div class="row g-4">
         <?php foreach ($products as $i => $product) : ?>
-            <div class="col">
-                <div class="border border-dark border-3 p-3 text-center">
-                    <p><?= $product['sku'] ?></p>
-                    <p><?= $product['name'] ?></p>
-                    <p><?= $product['price'] ?> $</p>
-                    <p><?= $product['value'] ?></p>
+            <div class="col-6 col-md-3">
+                <div class="card border-primary">
+                    <div class="card-body">
+                        <div class="form-check-inline">
+                            <label class="form-check-label">
+                                <input type="checkbox" class="form-check-input" name="products[]" value="${product.sku}">
+                            </label>
+                        </div>
+                        <p class="card-title text-center"><?= $product['sku'] ?></p>
+                        <p class="card-text text-center"><?= $product['name'] ?></p>
+                        <p class="card-text text-center"><?= $product['price'] ?> $</p>
+                        <p class="card-text text-center"><?= $product['value'] ?></p>
+                    </div>
                 </div>
             </div>
         <?php endforeach ?>
