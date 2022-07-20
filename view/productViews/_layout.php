a3d9fe9881bb708ad5c698f330c3385915a53fcd
Gunārs Ābeltiņš <gunars2003@gmail.com>
2022-07-20 21:46:33 +0300

    Add icon

--- view/productViews/_layout.php
+++ view/productViews/_layout.php
@@ -4,11 +4,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Product List</title>
+    <title>Product Website</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="/public/style.css">
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     <script defer src="/public/script.js"></script>
+    <link rel="icon" href="/public/favicon.png">
 </head>
 
 <body class="d-flex flex-column min-vh-100">
