526db3c712b6a7af1e3d688e51b6469538dd60d9
Gunārs Ābeltiņš <gunars2003@gmail.com>
2022-07-21 15:30:48 +0300

    Change database name

--- config/config.php
+++ config/config.php
@@ -3,6 +3,6 @@
 namespace app\config;
 
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'lamp_server');
+define('DB_NAME', 'products_server');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
